ce7fac63514d74e74fda4a90206642faae389221
BK Sivani <2025cs_boddukhanishkasivani_b@nie.ac.in>
2026-09-22 15:19:40 +0530

    pydantic model

--- Day/Day 2/FastAPIPractice/main.py
+++ Day/Day 2/FastAPIPractice/main.py
@@ -6,6 +6,7 @@
 #PATCH : Partial update
 #DELETE : Remove data
 from fastapi import FastAPI
+from pydantic import BaseModel
 app = FastAPI()
 @app.get("/")
 def home():
@@ -16,3 +17,23 @@ def about():
 @app.get("/health")
 def health():
     return {"status":"ok"}
+# POST request
+@app.post("/create")
+def create_something():
+    return {"message":"Created"}
+# Path parameters
+@app.get("/student/{usn}")
+def get_result(usn):
+    return {"Result":"Distinction","usn":usn}
+# Path paramters with type hint
+@app.get("/candidate/{rollno}}")
+def get_candidate(rollno:int):
+    return {"Result":"Distinction","rollno":rollno,"type":str(type(rollno))}
+# Pydantic model
+class Item(BaseModel):
+    name:str
+    price:float
+    in_stock: bool = True
+@app.post("/items")
+def create_item(item:Item):
+    return{"Recieved":item,"total_price":item.price*1.18}
